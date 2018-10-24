3d59b847829ab3e3c4ea040f4707f3f00560086d
nirt <nirt514@gmail.com>
2018-10-24 17:54:49 +0300

    functionality added

--- js/main.js
+++ js/main.js
@@ -7,12 +7,9 @@ function init() {
     ctx = canvas.getContext('2d');
     gCurrImg = document.getElementById('start-image');
 
-
 }
 
 function renderCanvas(img) {
-    // canvas.width = img.width;
-    // canvas.height = img.height;
     ctx.drawImage(img, 0, 0);
 }
 
@@ -27,110 +24,58 @@ function downloadImg(elLink) {
 
 function createMeme() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // gCurrImg = img
-
-    // renderCanvas(gCurrImg);
     renderCanvas(gCurrImg);
-
-    // Defaul style for text
-    ctx.lineWidth = 5;
-    ctx.font = '20pt impact';
-    ctx.strokeStyle = 'black';
-    ctx.fillStyle = 'white';
-    ctx.textAlign = 'center';
-    ctx.lineJoin = 'round';
-
-    // Draw the text
-    var text = document.getElementById('custom-text').value;
-    // text = text.toUpperCase();
-    // x = canvas.width/2 - gCurrImg.width/2;
-    // y = canvas.height/2 - gCurrImg.height/2;
-    // ctx.drawImage(gCurrImg, 0, 0);
-    // ctx.strokeText(text, 200, 50);
-    ctx.fillText(text, 200, 50);
-    // ctx.strokeText(text, x, y);
-    // ctx.fillText(text, x, y);
-}
-
-// function styleText(changeToMake) {
-
-//     switch (changeToMake) {
-//         case 'erase':
-//             renderCanvas(gCurrImg);
-//             break;
-//         case 'uppercase':
-//         renderCanvas(gCurrImg);
-//             var text = document.getElementById('custom-text').value;
-//             text = text.toUpperCase();
-//             // ctx.strokeText(text, 200, 50);
-//             ctx.fillText(text, 200, 50);
-//             break;
-//         case 'stroke':
-//         renderCanvas(gCurrImg);
-//             var text = document.getElementById('custom-text').value;
-//             ctx.strokeText(text, 200, 50);
-//             ctx.fillText(text, 200, 50);
-//             break;
-
-
-//     }
-// }
-
-
-// function styleText(changeToMake) {
-
-switch (changeToMake) {
-    case 'erase':
-        gMeme.line = '';
-        break;
-    case 'uppercase':
-        if (gMeme.uppercase === false) {
-            gMeme.line = gMeme.line.toUpperCase();
-            gMeme.uppercase = true;
-        } else {
-            gMeme.line = gMeme.line.toLowerCase();
-            gMeme.uppercase = false;
-        }
-        break;
-    case 'stroke':
-        if (gMeme.stroke === false) {
-            gMeme.stroke = true;
-        } else {
-            gMeme.stroke = false;
-        }
-        break;
-    case 'font-size-up':
-        gMeme.size += 1;
-        break;
-    case 'font-size-down':
-        gMeme.size -= 1;
-        break;
-    case 'align-txt-left':
-        gMeme.align = 'left';
-        break;
-    case 'align-txt-center':
-        gMeme.align = 'center';
-        break;
-    case 'align-txt-right':
-        gMeme.align = 'right';
-        break;
-    case 'line-select':
-        // To do- prep for optional 2nd line
-        break;
-    case 'color':
-        // To do- add color selection to button
-        break;
-    //To do - add rendering function by Meme obj
-
+    for (var i = 0; i <= gLineCounter; i++) {
+        let el = 'custom-text-' + i;
+        var text = document.getElementById(el).value;
+        // gMeme.txts[0].line = text;
+        // gMeme.txts[0].lineX = canvas.width / 2 - text.length * 6;
+        gMeme.txts[i].line = text;
+        gMeme.txts[i].lineX = canvas.width / 2 - text.length * 6;
+    }
+    updateInputStyle()
 }
 
 
-// img.onload = function () {
-//     // Work out where to center it
-//     x = canvas.width / 2 - img.width / 2;
-//     y = canvas.height / 2 - img.height / 2;
 
-//     // Draw it
-//     ctx.drawImage(img, x, y);
-// }
+function updateInputStyle() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    renderCanvas(gCurrImg);
+    for (var i = 0; i <= gLineCounter; i++) {
+        var text = gMeme.txts[i].line
+        ctx.lineWidth = 5;
+        ctx.lineJoin = 'round';
+        ctx.font = gMeme.txts[i].size + 'pt impact';
+        ctx.fillStyle = gMeme.txts[i].color;
+        ctx.textAlign = gMeme.txts[i].align;
+        if (gMeme.txts[i].stroke) {
+            ctx.strokeStyle = 'black';
+            ctx.strokeText(text, gMeme.txts[i].lineX, (gMeme.txts[i].lineY + i * 50));
+        }
+        ctx.fillText(text, gMeme.txts[i].lineX, (gMeme.txts[i].lineY + i * 50));
+    }
+
+}
+
+function addLine() {
+    gLineCounter += 1;
+    document.querySelector("#inputs").innerHTML += `
+    <p> <input id="custom-text-${gLineCounter}" type="text" placeholder="Enter Text Here" oninput="createMeme()"/></p>
+    `;
+    var newLine = {
+        line: 'Enter text',
+        lineX: 150,
+        lineY: 50,
+        uppercase: false,
+        stroke: true,
+        size: 20,
+        align: 'left',
+        color: "#ffffff"
+    };
+    gMeme.txts.push(newLine);
+    gCurrLine += 1;
+}
+
+function lineFocus() {
+
+}
--- js/service.js
+++ js/service.js
@@ -1,25 +1,107 @@
-
-
+var gCurrLine = 0;
+var gLineCounter = 0;
 var gImgs = [{ id: 1, url: '/meme-imgs/9.jpg', keywords: ['happy'] }];
 var gMeme = {
     selectedImgId: 1,
-    line: 'We\'ll make theme learn REGEX',
-    uppercase: false,
-    stroke: true,
-    size: 20,
-    align: 'left',
-    color: 'red'
+    txts: [
+        {
+            line: 'Enter text',
+            lineX: 150,
+            lineY: 50,
+            uppercase: false,
+            stroke: true,
+            size: 20,
+            align: 'left',
+            color: "#ffffff"
+        },
+    ]
 }
-    
-
 
 function selectedMeme(img) {
-
     gMeme.selectedImgId = img.id;
     gCurrImg = img;
-
-
     renderCanvas(img)
 }
 
+function deleteText() {
+    gMeme.txts[gCurrLine].line = '';
+    updateInputStyle()
+    let  elemId = 'custom-text-' + gCurrLine;
+    var elToReset = document.getElementById(elemId);
+    elToReset.value = '';
+
+}
+
+function toggleUppercase() {
+    gMeme.txts[gCurrLine].uppercase = !gMeme.txts[gCurrLine].uppercase;
+    if (gMeme.txts[gCurrLine].uppercase) gMeme.txts[gCurrLine].line = gMeme.txts[gCurrLine].line.toUpperCase();
+    else gMeme.txts[gCurrLine].line = gMeme.txts[gCurrLine].line.toLowerCase();
+    updateInputStyle()
+}
+
+function toggleStroke() {
+    gMeme.txts[gCurrLine].stroke = !gMeme.txts[gCurrLine].stroke;
+    updateInputStyle()
+}
+
+function fontResize(resize) {
+    gMeme.txts[gCurrLine].size += resize;
+    updateInputStyle()
+}
+
+function lineAlign(direction) {
+    gMeme.txts[gCurrLine].lineX += direction;
+    updateInputStyle()
+}
+
+function lineAlignCenter() {
+    gMeme.txts[gCurrLine].lineX = canvas.width / 2 - gMeme.txts[gCurrLine].line.length * 6;
+    gMeme.txts[gCurrLine].lineY = 50;
+    updateInputStyle()
+}
+
+function lineHeight(direction) {
+    gMeme.txts[gCurrLine].lineY += direction;
+    updateInputStyle()
+}
+
+// Color select button handler
+document.getElementById("color-wheel").addEventListener("change", watchColorPicker, false);
+
+function watchColorPicker(event) {
+    var colorSelector = document.querySelector("#color-wheel")
+    gMeme.txts[gCurrLine].color = colorSelector.value;
+    updateInputStyle()
+}
+
+
+function switchLine() {
+    var prevLine = gCurrLine;
+    let elId = 'custom-text-' + prevLine;
+    var elUnFocus = document.getElementById(elId);
+    elUnFocus.classList.remove("focus");
+    gCurrLine += 1;
+    if (gCurrLine > gLineCounter) gCurrLine = 0;
+    let  elemId = 'custom-text-' + gCurrLine;
+    var elToFocus = document.getElementById(elemId);
+    elToFocus.classList.add("focus");
+}
+
+function deleteLine() {
+    if (gLineCounter === 0) {
+        console.log('Cannot delete more lines!');
+        return
+    }
+    gMeme.txts.splice(gCurrLine,1);
+    gLineCounter -= 1;
+    let elId = 'custom-text-' + gCurrLine;
+    var elem = document.getElementById(elId);
+    elem.parentNode.removeChild(elem);
+    updateInputStyle()
+    gCurrLine -= 1;
+    if (gCurrLine < 0) gCurrLine = 0;
+    let  elemId = 'custom-text-' + gCurrLine;
+    var elToFocus = document.getElementById(elemId);
+    elToFocus.classList.add("focus");
+}
 
